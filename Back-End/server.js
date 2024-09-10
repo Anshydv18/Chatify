41f95a4a3698aa40a95248ee129c1f39235c6f91
Anshydv18 <anshgithub@gmail.com>
2024-09-11 02:19:53 +0530

    build command

--- Back-End/server.js
+++ Back-End/server.js
@@ -5,12 +5,14 @@ import messageRoutes from './routers/message.router.js'
 import userRoutes from './routers/user.router.js'
 import connectToMongoDB from './DB/connectDatabse.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 import cors from 'cors'
 import { app, server } from './socket/socket.js'
 
 dotenv.config();
 const PORT = process.env.PORT||5000
 
+const __dirname = path.resolve();
 
 const corsOptions = {
     origin: ["http://localhost:5173","*"], 
@@ -27,6 +29,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname, "/Front-End/dist")));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"Front-End","dist","index.html"))
+})
 server.listen(PORT,()=>{
     connectToMongoDB()
     console.log("server listening at port",PORT);
