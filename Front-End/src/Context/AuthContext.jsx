faf68ae45a82e37a9b8f2995947953f8d00037c3
Anshydv18 <anshgithub@gmail.com>
2024-09-11 02:26:00 +0530

    done

--- Front-End/src/Context/AuthContext.jsx
+++ Front-End/src/Context/AuthContext.jsx
@@ -10,6 +10,7 @@ export const AuthContextProvider = ({ children }) => {
   return (
     <AuthContext.Provider value={{ authUser, setAuthUser }}>
       {children}
+      
     </AuthContext.Provider>
   )
 };
